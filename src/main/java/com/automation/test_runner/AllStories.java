ff90e26e7bac91722a43dca57e413fe45f5f64f9
mbarghuthi <mbarghuthi@optimizasolutions.com>
2024-10-28 16:25:14 +0300

    Did some updates on for the latest release

--- src/main/java/com/automation/test_runner/AllStories.java
+++ src/main/java/com/automation/test_runner/AllStories.java
@@ -29,13 +29,13 @@ public class AllStories extends JBehaveReportFormatter {
 //				String.format("stories/**/%s.story", STORY_NAME.trim()), "");
 
 // to specify running single story / scenario
-//				String.format("stories/TC_006_UOP.story", STORY_NAME.trim()), "");
+				String.format("stories/TC_003_UOP.story", STORY_NAME.trim()), "");
 
 //Run a only for single test scenario
 //				String.format("stories/TC_004_SCC.story", STORY_NAME.trim()), "");
 
 //Run a only UOP stories
-				String.format("stories/**/%s.story", STORY_NAME_PATTERN_UOP.trim()), "");
+//				String.format("stories/**/%s.story", STORY_NAME_PATTERN_UOP.trim()), "");
 
 //Run a only SCC stories
 //		String.format("stories/**/%s.story", STORY_NAME_PATTERN_SCC.trim()), "");
